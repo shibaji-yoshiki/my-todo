5945f65a441b0c6cbba3ffbd1d0b8183b7f89798
SHIBAJI <shibaji-yoshiki>
2022-06-28 12:18:56 +0900

    TODO レイアウト

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import TheFooter from './components/TheFooter.vue';
+import TheHeader from './components/TheHeader.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <!-- <div>ヘッダー</div> -->
+  <div class="wrap">
+    <TheHeader />
+    <main class="main">メイン</main>
+    <!-- <div>フッター</div> -->
+    <TheFooter />
+  </div>
 </template>
 
 <style>
-#app {
+@import 'assets/css/destyle.css';
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  width: 370px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+
+.main {
+  flex: 1;
+  width: 100%;
 }
 </style>
